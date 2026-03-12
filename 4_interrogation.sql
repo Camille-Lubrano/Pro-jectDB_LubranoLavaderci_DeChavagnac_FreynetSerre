be17bb0da632651509651390a43b18ed9f3cb6db
Camille.Lubrano <camille.lubrano-lavaderci@efrei.net>
2026-03-12 16:48:09 +0100

    Update queries
    Added SQL queries to list prices below 4€, unique categories, and product ingredients.

--- 4_interrogation.sql
+++ 4_interrogation.sql
@@ -1,3 +1,14 @@
+-- Lists prices bellow 4€
+SELECT Pr_Name, Price
+FROM Product
+WHERE Price < 4
+ORDER BY Price DESC;
+
+-- Lists unique categories 
+SELECT DISTINCT Pr_Category
+FROM Product;
+
+-- Lists ingredients of product
 SELECT i.Ing_name
 FROM ingredient i
 JOIN has h ON i.ID_Ingredient = h.ID_Ingredient 
