e88b6bfbec63a82a7dbce61ce7493d82c341d676
Camille.Lubrano <camille.lubrano-lavaderci@efrei.net>
2026-03-12 16:50:40 +0100

    Update.2 queries
    Added queries to list products within a calorie range and products containing 'cake' in their name.

--- 4_interrogation.sql
+++ 4_interrogation.sql
@@ -8,6 +8,16 @@ ORDER BY Price DESC;
 SELECT DISTINCT Pr_Category
 FROM Product;
 
+-- Lists products between 300 and 400
+SELECT Pr_Name, Pr_Calories
+FROM Product
+WHERE Pr_ Calories BETWEEN 300 AND 400;
+
+-- Lists products with cake in their name
+SELECT *
+FROM Product
+WHERE Pr_Name LIKE '%cake%';
+
 -- Lists ingredients of product
 SELECT i.Ing_name
 FROM ingredient i
